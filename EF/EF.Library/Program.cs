cc2851d8718dbd349189806a5d3f7d9625f52fa6
valmon55 <fedor_ka@mail.ru>
2023-11-23 15:43:49 +0300

    25.4.3 correct

--- EF/EF.Library/Program.cs
+++ EF/EF.Library/Program.cs
@@ -9,7 +9,7 @@ namespace EF.Library
         {
             using (var db = new AppContext())
             { 
-                UserRepository userRepository = new UserRepository();   
+                //UserRepository userRepository = new UserRepository();   
 
                 User jane = new User() { Name = "Jane" };
                 User neo = new User() { Name = "Neo" };
@@ -24,21 +24,21 @@ namespace EF.Library
 
                 //db.Books.AddRange(tennis, fantasy, cs_docs);
 
-                userRepository.AcceptBook(jane, tennis);
-                userRepository.AcceptBook(jane, cs_docs);
-                userRepository.AcceptBook(jane, oracle);
-                userRepository.AcceptBook(neo, fantasy);
-                userRepository.AcceptBook(neo, tennis);
-                userRepository.AcceptBook(neo, fantasy);
+                //userRepository.AcceptBook(jane, tennis);
+                //userRepository.AcceptBook(jane, cs_docs);
+                //userRepository.AcceptBook(jane, oracle);
+                //userRepository.AcceptBook(neo, fantasy);
+                //userRepository.AcceptBook(neo, tennis);
+                //userRepository.AcceptBook(neo, fantasy);
 
-                ////jane.books.AddRange(tennis, cs_docs);
-                //jane.books.Add(cs_docs);
-                //jane.books.Add(tennis);
+                //jane.books.AddRange(tennis, cs_docs);
+                jane.books.Add(cs_docs);
+                jane.books.Add(tennis);
                 ////tennis.User = jane;
 
-                ////neo.books.AddRange(fantasy, oracle);
-                //neo.books.Add(fantasy);
-                //neo.books.Add(oracle);
+                //neo.books.AddRange(fantasy, oracle);
+                neo.books.Add(fantasy);
+                neo.books.Add(oracle);
 
                 db.SaveChanges();
             }
